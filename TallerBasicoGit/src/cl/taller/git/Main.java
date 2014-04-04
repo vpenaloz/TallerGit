8ae28a5e10d9fbab39ea66f5faf08093b380befc
vpenaloz <vpenaloz@alumnos.ubiobio.cl>
2014-04-04 10:55:03 -0400

    tercero

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Segundo Commit");
-		System.out.println("Linea agregada por web");
+		System.out.println("Linea agregada por web xcgxhcghxchxch");
 
 	}
 
